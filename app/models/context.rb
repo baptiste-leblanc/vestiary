501ee4ce0667962c44ff57fbff91687d132ac7f2
Ugo Baquian <ubaquian@gmail.com>
2025-08-19 17:33:29 +0200

    Piece page created with seed data

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,4 +1,5 @@
 class Context < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
+  has_many :looks, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,40 @@
 #   end
 
 # Supprimer toutes les entrées de la seed
-Context.destroy_all
+LookPiece.destroy_all
 Look.destroy_all
+Message.destroy_all
+Context.destroy_all
 Piece.destroy_all
+User.destroy_all
 
 puts "Everything is destroyed"
+
 # Créer un contexte
 
-user = User.new(name: "Ugo", email: "test@test.com", password: "password", gender: "male")
+user = User.new(username: "ugo", email: "test@test.com", password: "password", gender: "male")
 user.save
 puts "User created"
 
-context = Context.new(style: "Je recherche un vêtement pour un mariage", budget: "1000€ max")
+context = Context.new(objective: "Je recherche un vêtement pour un mariage", budget: "1000€ max", user_id: User.first.id)
 context.save
 puts "Context created"
 
-look = Look.new(description: "Costume classique", name: "")
-piece = Piece.new()
+look = Look.new(description: "Costume classique desc", name: "Costume classique", context_id: Context.first.id)
+look.save
+puts "look created"
+
+piece = Piece.new(clothing_category: "scarf", name: "Red scarf", description: "Red scarf desc", price: "10€", brand: "Ralph Lauren", shop_url: "www.google.com")
+piece.save
+puts "piece created"
+piece = Piece.new(clothing_category: "pull", name: "Red pull", description: "Red pull desc", price: "10€", brand: "Ralph Lauren", shop_url: "www.google.com")
+piece.save
+puts "piece created"
+
+look_piece = LookPiece.new(look_id: Look.first.id, piece_id: Piece.first.id)
+look_piece.save
+puts "look piece created"
+
+look_piece = LookPiece.new(look_id: Look.first.id, piece_id: Piece.last.id)
+look_piece.save
+puts "look piece created"
