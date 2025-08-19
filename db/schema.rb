9d70fa3b5302a10c91685cc5a9122ab2936f9429
Gael Roussel <gael.roussel@mac.com>
2025-08-19 14:57:56 +0200

    user_table_update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_19_101313) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_19_125338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,8 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_101313) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "mail"
-    t.string "password"
     t.float "weight"
     t.float "height"
     t.string "gender"
