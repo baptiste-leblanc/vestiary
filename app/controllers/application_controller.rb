940a9e0f46ea7b48046837ef0f9b9f3063121eed
Gael Roussel <gael.roussel@mac.com>
2025-08-19 18:00:34 +0200

    signUp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :height, :weight, :gender])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :height, :weight, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :height, :weight, :gender, :photos])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :height, :weight, :gender, :photos])
   end
 end
