192ea5d1f888ce7d6e2477d461b6124f72f8aa09
Baptiste Leblanc <baptisteleblanc8@gmail.com>
2025-08-20 11:09:41 +0200

    Pull cloudinary

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -1,10 +1,32 @@
 class ContextsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @user = User.find(params[:user_id])
+    @looks = Look.all
+    @contexts = Context.all
+    @context = Context.new
+  end
+
   def new
     @context = Context.new
   end
 
-  def index
-    @looks = Look.all
-    @contexts = Context.all
+  def create
+    @context = Context.new(context_params)
+    @context.user = @user
+
+    if @context.save
+      redirect_to user_contexts_path(@user)
+    else
+      @contexts = @user.contexts
+      render :index, status: :unprocessable_entity
+    end
   end
+
+  # private
+
+  # def context_params
+  #   params.require(:context).permit(:objective, :budget)
+  # end
 end
