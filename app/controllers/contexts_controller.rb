64d5f5b7796172b887f46efb63a6d36c129ad88c
Baptiste Leblanc <baptisteleblanc8@gmail.com>
2025-08-20 18:10:56 +0200

    Up

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -5,12 +5,26 @@ class ContextsController < ApplicationController
     @looks = Look.all
     @look = Look.new
     # redirect_to context_look_path(@look)
-    # /contexts/:context_id/looks/:id(.:format)
+    chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: { type: 'json_object' })
+    system_prompt = "You are a professional personal shopper.
+        Your role is to help a client choose look that best match their needs.
+
+        Client profile:
+        The look of the client have to be coherent to why he wants to relook, fit the morphology.
+
+        Task:
+        Generate up to 3 looks items that form a coherent, stylish look for this client and event.
+        For these looks, you have to give a name for this outfit and a short description of the look and why it fits perfectly with the client. Don't give the items of the look.
+
+        Output format:
+        Return the result as a JSON object with exactly the fields above. Do not include extra text before or after the JSON."
+    response = chat.ask(system_prompt)
+    @contexts = JSON.parse(response.content)
   end
 
   def index
-    @contexts = Context.all
     @context = Context.new
+    @contexts = Context.all
     @looks = Look.all
   end
 
