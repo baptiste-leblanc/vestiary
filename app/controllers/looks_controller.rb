e0560282a0b2b05abe2f120c484b3cba9f9be4e5
Ugo Baquian <ubaquian@gmail.com>
2025-08-20 15:10:02 +0200

    CHanges from Gael

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -5,29 +5,30 @@ class LooksController < ApplicationController
 
   def show
     @look = Look.find(params[:id])
-    # chat = RubyLLM.chat(model: "gpt-4o")
-    # system_prompt = "You are a professional personal shopper.
-    #   Your role is to help a client choose clothing pieces that best match their needs.
+    chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: {type: 'json_object'})
+    system_prompt = "You are a professional personal shopper.
+      Your role is to help a client choose clothing pieces that best match their needs.
 
-    #   Client profile:
-    #   - Objective: wedding, old-fashioned style
-    #   - Morphology: large build
-    #   - Budget: medium
+      Client profile:
+      - Objective: wedding with #{@look.name}
+      - Morphology: large build
+      - Budget: medium
 
-    #   Task:
-    #   Generate up to 5 clothing items that form a coherent, stylish outfit for this client and event. Each item should include realistic values for:
-    #   clothing_category, name, description, price, brand, shop_url.
+      Task:
+      Generate up to 5 clothing items that form a coherent, stylish outfit for this client and event. Each item should include realistic values for:
+      clothing_category, name, description, price, brand, shop_url.
 
-    #   Constraints:
-    #   - Fit the wedding context (formal, elegant)
-    #   - Adapted to large morphology (comfortable, flattering cuts)
-    #   - Stay within medium budget
+      Constraints:
+      - Fit the wedding context (formal, elegant)
+      - Adapted to large morphology (comfortable, flattering cuts)
+      - Stay within medium budget
 
-    #   Output format:
-    #   Return the result as a Ruby hash with exactly the fields above. Do not include extra text before or after the hash. Do not put 'ruby\n[\n' at the beggining of the text"
-    #   response = chat.ask(system_prompt)
-      @pieces = @look.pieces
-end
+      Output format:
+      Return the result as a JSON object with exactly the fields above. Do not include extra text before or after the JSON."
+      response = chat.ask(system_prompt)
+      @pieces = JSON.parse(response.content)
+    end
+    # @pieces = @look.pieces
 # chat.with_instructions(system_prompt)
 # prompt = "Here is the Json with the list of clothing pieces you can use:"
 # response = chat.ask(prompt, with: {json: "db/pieces_500_real.json"})
