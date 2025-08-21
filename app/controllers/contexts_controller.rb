da8e963504aeb1ef828d81694ab7612c394e7c9a
Baptiste Leblanc <baptisteleblanc8@gmail.com>
2025-08-21 12:47:50 +0200

    Big push back-end

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -1,31 +1,8 @@
 class ContextsController < ApplicationController
-  def show
-    @context = Context.new
-    # @context = Context.find(params[:id])
-    @looks = Look.all
-    @look = Look.new
-    # redirect_to context_look_path(@look)
-    chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: { type: 'json_object' })
-    system_prompt = "You are a professional personal shopper.
-        Your role is to help a client choose look that best match their needs.
-
-        Client profile:
-        The look of the client have to be coherent to why he wants to relook, fit the morphology.
-
-        Task:
-        Generate up to 3 looks items that form a coherent, stylish look for this client and event.
-        For these looks, you have to give a name for this outfit and a short description of the look and why it fits perfectly with the client. Don't give the items of the look.
-
-        Output format:
-        Return the result as a JSON object with exactly the fields above. Do not include extra text before or after the JSON."
-    response = chat.ask(system_prompt)
-    @contexts = JSON.parse(response.content)
-  end
 
   def index
     @context = Context.new
     @contexts = Context.all
-    @looks = Look.all
   end
 
   def create
@@ -33,10 +10,35 @@ class ContextsController < ApplicationController
     @context.user = current_user
 
     if @context.save
+
       redirect_to context_path(@context)
     else
       render :index, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @context = Context.find(params[:id])
+    chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: { type: 'json_object' })
+    system_prompt = 'You are a professional personal shopper.
+              Your role is to help a client choose looks that best match their needs.
+
+              Client profile:
+              The looks must be coherent with the input fields: objective and morphology.
+
+              Task:
+              Generate exactly 3 different stylish looks for this client and event.
+              For each look, provide:
+              - a name
+              - a short description explaining why it fits perfectly with the client.
+              Do not list the individual clothing items.
+
+              Output format:
+              Return the result as a JSON with a "looks" that contains an array of 3 objects.
+              Each object must have two fields: "name" and "description".
+              Do not include extra text before or after the JSON.'
+    response = chat.with_instructions(system_prompt).ask(:objective)
+    @looks = JSON.parse(response.content)["looks"]
   end
 
   private
