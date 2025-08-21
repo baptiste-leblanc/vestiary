bde89fe93866971b78218fbc8f527a24df135dc5
Ugo Baquian <ubaquian@gmail.com>
2025-08-21 15:16:49 +0200

    Images added

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -1,49 +1,49 @@
 class ContextsController < ApplicationController
-
   def index
     @context = Context.new
     @contexts = Context.all
   end
-
   def create
     @context = Context.new(context_params)
     @context.user = current_user
-
     if @context.save
-
+      generate_looks
       redirect_to context_path(@context)
     else
       render :index, status: :unprocessable_entity
     end
   end
-
   def show
     @context = Context.find(params[:id])
+    @looks = @context.looks
+  end
+  private
+  def context_params
+    params.require(:context).permit(:objective, :budget)
+  end
+  def create_looks(array)
+    @looks = array.map do |element|
+      Look.create!(description: element["description"], name: element["name"], context: @context)
+    end
+  end
+  def generate_looks
     chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: { type: 'json_object' })
     system_prompt = 'You are a professional personal shopper.
               Your role is to help a client choose looks that best match their needs.
-
               Client profile:
               The looks must be coherent with the input fields: objective and morphology.
-
               Task:
               Generate exactly 3 different stylish looks for this client and event.
               For each look, provide:
               - a name
               - a short description explaining why it fits perfectly with the client.
               Do not list the individual clothing items.
-
               Output format:
               Return the result as a JSON with a "looks" that contains an array of 3 objects.
               Each object must have two fields: "name" and "description".
               Do not include extra text before or after the JSON.'
-    response = chat.with_instructions(system_prompt).ask(:objective)
-    @looks = JSON.parse(response.content)["looks"]
-  end
-
-  private
-
-  def context_params
-    params.require(:context).permit(:objective, :budget)
+    response = chat.with_instructions(system_prompt).ask(@context.objective)
+    looks_payload = JSON.parse(response.content)["looks"]
+    create_looks(looks_payload)
   end
 end
--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -8,36 +8,26 @@ class LooksController < ApplicationController
     morphology = "large build"
     chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: {type: 'json_object'})
     system_prompt = "You are a professional personal shopper.
-      Your role is to help a client choose clothing pieces that best match their needs.
+    Your role is to help a client choose clothing pieces that best match their needs.
 
-      Client profile:
-      - Objective: wants pieces for this style: #{@look.name}
-      - Morphology: #{morphology}
-      - Budget: #{@look.context['budget']}
+    Client profile:
+    - Objective: wants pieces for this style: #{@look.name}
+    - Morphology: #{morphology}
+    - Budget: #{@look.context['budget']}
 
-      Task:
-      Generate up to 5 clothing items that form a coherent, stylish outfit for this client and event. Each item should include realistic values for:
-      clothing_category, name, description, price (without currency sign), brand, shop_url.
+    Task:
+    Generate up to 5 clothing items that form a coherent, stylish outfit for this client and event. Each item should include realistic values for:
+    clothing_category, name, description, price (without currency sign), brand, shop_url, unsplash_query.
 
-      Constraints:
-      - Fit the wedding context (formal, elegant)
-      - Adapted to large morphology (comfortable, flattering cuts)
-      - Stay within medium budget
+    Constraints:
+    - Fit the style objective
+    - Adapted to large morphology (comfortable, flattering cuts)
+    - Stay within medium budget
+    - Have the relevant keywords for an image query in unsplash
 
-      Output format:
-      Return the result as a JSON object with exactly the fields above. Do not include extra text before or after the JSON."
-      response = chat.ask(system_prompt)
-      @pieces = JSON.parse(response.content)
-
-      api_key = ENV["UNSPLASH_API_KEY"]
-
-      url = "https://api.unsplash.com/search/photos?client_id=#{api_key}&page=1&query=office"
-      image_serialized = URI.parse(url).read
-      image = JSON.parse(image_serialized)
-      raise
-    end
-# @pieces = @look.pieces
-# chat.with_instructions(system_prompt)
-# prompt = "Here is the Json with the list of clothing pieces you can use:"
-# response = chat.ask(prompt, with: {json: "db/pieces_500_real.json"})
+    Output format:
+    Return the result as a JSON object with exactly the fields above. Do not include extra text before or after the JSON."
+    response = chat.ask(system_prompt)
+    @pieces = JSON.parse(response.content)
+  end
 end
