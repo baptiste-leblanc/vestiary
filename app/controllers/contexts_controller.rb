83d042cd6b5e8e7fa25ffc7ae817875fb017233d
Baptiste Leblanc <baptisteleblanc8@gmail.com>
2025-08-21 15:05:57 +0200

    Push back-end

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -9,7 +9,7 @@ class ContextsController < ApplicationController
     @context.user = current_user
 
     if @context.save
-
+      generate_looks
       redirect_to context_path(@context)
     else
       render :index, status: :unprocessable_entity
@@ -18,6 +18,22 @@ class ContextsController < ApplicationController
 
   def show
     @context = Context.find(params[:id])
+    @looks = @context.looks
+  end
+
+  private
+
+  def context_params
+    params.require(:context).permit(:objective, :budget)
+  end
+
+  def create_looks(array)
+    @looks = array.map do |element|
+      Look.create!(description: element["description"], name: element["name"], context: @context)
+    end
+  end
+
+  def generate_looks
     chat = RubyLLM.chat(model: "gpt-4o").with_params(response_format: { type: 'json_object' })
     system_prompt = 'You are a professional personal shopper.
               Your role is to help a client choose looks that best match their needs.
@@ -36,13 +52,9 @@ class ContextsController < ApplicationController
               Return the result as a JSON with a "looks" that contains an array of 3 objects.
               Each object must have two fields: "name" and "description".
               Do not include extra text before or after the JSON.'
-    response = chat.with_instructions(system_prompt).ask(:objective)
-    @looks = JSON.parse(response.content)["looks"]
-  end
-
-  private
-
-  def context_params
-    params.require(:context).permit(:objective, :budget)
+    # prompt = "Objective:" @context.objective &"/n"&"Morphology: Fat"
+    response = chat.with_instructions(system_prompt).ask(@context.objective)
+    looks_payload = JSON.parse(response.content)["looks"]
+    create_looks(looks_payload)
   end
 end
